60d08dcf90625048fc7493fa9a5744a259bb55d3
Dylan <martind27@hanover.edu>
2025-10-20 11:00:18 -0400

    Added class fields

--- app/src/main/java/core/actions/SelectAction.java
+++ app/src/main/java/core/actions/SelectAction.java
@@ -1,5 +1,9 @@
 package core.actions;
+import core.Card; // Card class import
+import core.locations.Location; // import the interface so that we can use the different locations
 
 public class SelectAction implements Action {
-    String test;
+    // We need the card they have selected and a location where that they have selected from
+    private Card selectedCard;
+    private Location fromLocation;
 }
