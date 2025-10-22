0a168c820e2481872ffb8142b8cad22b1dcd56aa
Dylan <martind27@hanover.edu>
2025-10-22 14:51:56 -0400

    Removed the old locations class files and removed Action.java class

--- app/src/main/java/core/locations/Waste.java
+++
@@ -1,9 +0,0 @@
-package core.locations;
-import core.Card;
-import java.util.List;
-import java.util.ArrayList;
-
-public class Waste implements Location {
-    // waste only needs to contain the cards that we have discarded
-    private List<Card> cards;
-}
