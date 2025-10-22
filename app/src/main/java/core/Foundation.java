074012cebb327724ef634f3d6bc84bcc383eb12b
Dylan <martind27@hanover.edu>
2025-10-22 15:47:40 -0400

    Implement basic Foundation class methods. ref #3

--- app/src/main/java/core/Foundation.java
+++ app/src/main/java/core/Foundation.java
@@ -1,7 +1,40 @@
 package core;
 
-public class Foundation {
-    public Foundation() {
+import com.google.common.hash.HashCode;
 
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Foundation {
+    private Map<Suit, List<Card>> piles;
+
+    public Foundation() {
+        piles = new HashMap<>();
+        for (Suit suit : Suit.values()) {
+            List<Card> cards = new ArrayList<>();
+            piles.put(suit, cards);
+        }
+    }
+    public boolean isEmpty(Suit suit) {
+        return piles.get(suit).isEmpty();
+    }
+
+    public void add(Card card) {
+        if (canPlaceCard(card)) {
+            piles.get(card.getSuit()).add(card);
+        }
+    }
+
+    private boolean canPlaceCard(Card card) {
+        Suit suit = card.getSuit();
+        int rank = card.getRank();
+        List<Card> pile = piles.get(suit);
+        if (pile.isEmpty()) {
+            return rank == 1;
+        }
+        return false;
     }
 }
--- app/src/test/java/core/FoundationTests.java
+++ app/src/test/java/core/FoundationTests.java
@@ -8,7 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FoundationTests {
     @Test
-    void FoundationTest() {
+    void initialFoundationPilesAreEmpty() {
+        Foundation emptyFoundation = new Foundation();
+        assertEquals(true, emptyFoundation.isEmpty(HEARTS));
+        assertEquals(true, emptyFoundation.isEmpty(SPADES));
+        assertEquals(true, emptyFoundation.isEmpty(DIAMONDS));
+        assertEquals(true, emptyFoundation.isEmpty(CLUBS));
+    }
 
+    @Test
+    void canAddAces() {
+        Foundation foundation = new Foundation();
+        Card card = new Card(1, HEARTS);
+        foundation.add(card);
+        assertEquals(false, foundation.isEmpty(HEARTS));
+        assertEquals(true, foundation.isEmpty(SPADES));
+        assertEquals(true, foundation.isEmpty(DIAMONDS));
+        assertEquals(true, foundation.isEmpty(CLUBS));
+    }
+
+    @Test
+    void canOnlyAddAcesToEmptyPiles() {
+        Foundation foundation = new Foundation();
+        Card card = new Card(2, HEARTS);
+        foundation.add(card);
+        assertEquals(true, foundation.isEmpty(HEARTS));
     }
 }
