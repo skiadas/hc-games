b367022d0c61a2191a385ebc23f8643517b7344f
Dylan <martind27@hanover.edu>
2025-10-22 14:46:58 -0400

    Update removed files from locations

---
+++ app/src/test/java/core/FoundationTests.java
@@ -0,0 +1,14 @@
+package core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import static core.Suit.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FoundationTests {
+    @Test
+    void FoundationTest() {
+
+    }
+}
